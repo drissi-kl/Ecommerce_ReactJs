2c7ad5e353c768020581c95b36d8e5c28cbab0d5
drissi-kl <drissiabderrahmane999@gmail.com>
2026-07-18 22:06:09 +0100

    update reviews component

--- src/components/home/review.jsx
+++ src/components/home/review.jsx
@@ -1,13 +1,16 @@
+import { Star } from 'lucide-react'
 import React from 'react'
 
 export default function Review() {
 
     const reviewsData = [
-        {name: "Eleanor Collins", comment:"Highly impressed!", rating: 5},
+        {name: "Eleanor Collins", comment:"Highly impressed!", rating: 1},
         {name: "Lucas Gordon", comment:"Highly impressed!", rating: 3},
         {name: "Liam Smith", comment:"Highly impressed!", rating: 5},
         {name: "Clara Berry", comment:"Highly impressed!", rating: 4},
     ]
+
+
 
   return (<section className='review'>
 
@@ -17,9 +20,28 @@ export default function Review() {
     <div className='review-cards'>
         {
             reviewsData.map((rd, index)=>{return <div key={index} className="review-card">
-                <p className='review-card-name'> {rd.name} </p>
+                <p className='review-card-rating'> 
+                    {
+                        rd.rating == 1 ? <>
+                            <Star className='star-icon' />
+                        </> 
+                        : rd.rating == 2 ? <>
+                            <Star className='star-icon' /><Star className='star-icon' />
+                        </>
+                        : rd.rating == 3 ? <>
+                            <Star className='star-icon' /><Star className='star-icon' /><Star className='star-icon' />
+                        </>
+                        : rd.rating == 4 ? <>
+                            <Star className='star-icon' /><Star className='star-icon' /><Star className='star-icon' /><Star className='star-icon' />
+                        </>
+                        :<>
+                            <Star className='star-icon' /><Star className='star-icon' /><Star className='star-icon' /><Star className='star-icon' /><Star className='star-icon' />
+                        </>
+                        
+                    }    
+                </p>
                 <p className='review-card-comment'> {rd.comment} </p>
-                <p className='review-card-rating'> {rd.rating} </p>
+                <p className='review-card-name'> {rd.name} </p>
             </div>})
         }
     </div>
