19b6935cae4dc1a51c6d424cd45743a7f3a0a1a7
drissi-kl <drissiabderrahmane999@gmail.com>
2026-08-14 22:54:42 +0100

    update relatedProducts component

--- src/components/productDetails/relatedProducts.jsx
+++ src/components/productDetails/relatedProducts.jsx
@@ -1,41 +1,57 @@
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+
 import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import "./relatedProducts.css";
-import { useEffect, useState } from "react";
+
 import { getProductsApi } from "../../services/products";
 
-export default function RelatedProducts({category}){
+export default function RelatedProducts({ category }) {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([]);
-    useState(()=>{
-        const fetchProducts = async () => {
-            try{
-                const response = await getProductsApi(100, 0, category);
-                setProducts(response.products);
-            }catch(error){
-                console.log(error);
-            }
-        }
-        fetchProducts();
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await getProductsApi(10, 0, category);
+        setProducts(response.products);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-        console.log('category', category);
-    }, [category])
+    fetchProducts();
+  }, [category]);
 
-
-    return <section className="related_products_section">
-        <h2 className="section_title">You Might Also Like</h2>
-        <Swiper className="products"
-            slidesPerView={3}
-            spaceBetween={10}
-        >
-            {products.map((relProduct) => {return <SwiperSlide className="product_card" key={relProduct.id}>
-                <img src={relProduct.thumbnail} alt={relProduct.title} />
-                <span>{relProduct.category}</span>
-                <h3>{relProduct.title}</h3>
-                <p>${relProduct.price}</p>
-                <button>Add to Cart</button>
-            </SwiperSlide>})}
-
-        </Swiper>
-      </section>
+  return (
+    <section className="related_products_section">
+      <h2 className="section_title">You Might Also Like</h2>
+      
+      <Swiper
+        className="products_swiper"
+        spaceBetween={20}
+        slidesPerView={3}
+        modules={[Navigation, Pagination]}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        
+      >
+        {products.map((relProduct) => (
+          <SwiperSlide key={relProduct.id} className="product_slide">
+            <div className="product_card">
+              <img src={relProduct.thumbnail} alt={relProduct.title} />
+              <span>{relProduct.category}</span>
+              <h3>{relProduct.title}</h3>
+              <p>${relProduct.price}</p>
+              <button>Add to Cart</button>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 }
